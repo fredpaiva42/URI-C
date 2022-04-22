022515f185363cab5eca58ac8ec979d1a8dd277a
fredpaiva42 <fredericogdpaiva@gmail.com>
2022-04-22 12:32:07 -0300

    22/04/2022

---
+++ Iniciante/ProdutoSimples.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int ProdutoSimples() {
+
+    int x, y;
+
+    scanf("%d", &x);
+    scanf("%d", &y);
+
+    int PROD = x * y;
+
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
---
+++ Iniciante/SomaSimples.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int SomaSimples() {
+
+    int a, b;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    int soma = a + b;
+
+    printf("SOMA = %d\n", soma);
+
+    return 0;
+}
