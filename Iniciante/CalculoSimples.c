6ce06b183e5e39d2bceae63f7697e91230a2ce7a
fredpaiva42 <fredericogdpaiva@gmail.com>
2022-04-28 09:38:16 -0300

    28/04/2022

---
+++ Iniciante/CalculoSimples.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int id, qtd, i;
+    double valor, total = 0.0;
+
+    for(i = 0; i < 2; i++) {
+        scanf("%d %d %lf", &id, &qtd, &valor);
+        total += qtd * valor;
+    }
+
+    printf("VALOR A PAGAR: R$ %.2lf\n", total);
+
+    return 0;
+}
