6b43ee5379a8ce6b7b92c1a581f633cf2294d144
fredpaiva42 <fredericogdpaiva@gmail.com>
2022-05-25 17:22:40 -0300

    25/05/2022

--- Iniciante/Selecao/FormulaDeBhaskara.c
+++ Iniciante/Selecao/FormulaDeBhaskara.c
@@ -12,7 +12,6 @@ int FormulaDeBhaskara() {
     if (delta < 0 || A == 0)
         printf("Impossivel calcular\n");
     else {
-
         R1 = (-B + sqrt(delta))/(2*A);
         R2 = (-B - sqrt(delta))/(2*A);
         printf("R1 = %.5lf\n", R1);
