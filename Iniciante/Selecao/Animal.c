02a3acf3019067d05b02e2240b537a1bf839c5ce
fredpaiva42 <fredericogdpaiva@gmail.com>
2022-06-20 11:00:27 -0300

    20/06/2022

---
+++ Iniciante/Selecao/Animal.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int Animal() {
+
+    char caracteristica1[20], caracteristica2[20], caracteristica3[20];
+
+    scanf("%s", caracteristica1);
+    scanf("%s", caracteristica2);
+    scanf("%s", caracteristica3);
+
+    if (strcmp(caracteristica1, "vertebrado") == 0){
+        if (strcmp(caracteristica2, "ave") == 0){
+            if (strcmp(caracteristica3, "carnivoro") == 0)
+                printf("aguia\n");
+            else
+                printf("pomba\n");
+        }else {
+            if (strcmp(caracteristica3, "onivoro") == 0)
+                printf("homem\n");
+            else
+                printf("vaca\n");
+        }
+    }else {
+        if (strcmp(caracteristica2, "inseto") == 0){
+            if (strcmp(caracteristica3, "hematofago") == 0)
+                printf("pulga\n");
+            else
+                printf("lagarta\n");
+        }else {
+            if (strcmp(caracteristica3, "onivoro") == 0)
+                printf("minhoca\n");
+            else
+                printf("sanguessuga\n");
+        }
+    }
+
+    return 0;
+}
