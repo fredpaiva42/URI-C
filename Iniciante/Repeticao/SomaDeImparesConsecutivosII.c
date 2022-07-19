abbaf844f443eaf7201f8fa271734e193869edfb
fredpaiva42 <fredericogdpaiva@gmail.com>
2022-07-19 12:29:51 -0300

    19/07/2022

---
+++ Iniciante/Repeticao/SomaDeImparesConsecutivosII.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+
+    int qtd_casos, x, y, maior, menor;
+
+    scanf("%d", &qtd_casos);
+
+    for (int i = 0; i < qtd_casos; i++) {
+        int soma = 0;
+
+        scanf("%d %d", &x, &y);
+
+        if (x > y){
+            maior = x;
+            menor = y;
+        }else {
+            maior = y;
+            menor = x;
+        }
+
+        for (menor++; menor < maior; menor++){
+            if (menor % 2 != 0){
+                soma += menor;
+            }
+        }
+
+        printf("%d\n", soma);
+    }
+
+    return 0;
+}
