2db602beb4744557274182a44f93f3872d3e02de
fredpaiva42 <fredericogdpaiva@gmail.com>
2022-07-26 11:18:09 -0300

    26/07/2022

---
+++ Iniciante/Repeticao/ValidacaoDeNota.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int ValidacaoDeNota() {
+
+    double notas, media = 0;
+    int contador = 0;
+
+    while (contador != 2){
+        scanf("%lf", &notas);
+
+        if (notas >= 0 && notas <= 10) {
+            media += notas;
+            contador++;
+        }else
+            printf("nota invalida\n");
+
+    }
+
+    printf("media = %.2lf\n", (media/2));
+
+    return 0;
+}
