0c69cce8f8c5a720993f4a6ca2438dbf65844417
fredpaiva42 <fredericogdpaiva@gmail.com>
2022-08-01 11:17:56 -0300

    01/08/2022

---
+++ Iniciante/Repeticao/VariasNotasComValidacao.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+
+    double notas, media = 0;
+    int querContinuar = 1, contador = 0;
+
+    while (querContinuar != 2){
+
+        switch(querContinuar){
+            case 1:
+                contador = 0;
+                media = 0;
+                while (contador != 2){
+                    scanf("%lf", &notas);
+
+                    int dentroDoIntervalo = notas >= 0 && notas <= 10;
+
+                    if (dentroDoIntervalo){
+                        contador++;
+                        media += notas;
+                    }else {
+                        printf("nota invalida\n");
+                    }
+                }
+                printf("media = %.2f\n", (media/2));
+                printf("novo calculo (1-sim 2-nao)\n");
+                scanf("%d", &querContinuar);
+                break;
+            case 2:
+                querContinuar = 2;
+                break;
+            default:
+                printf("novo calculo (1-sim 2-nao)\n");
+                scanf("%d", &querContinuar);
+                break;
+        }
+    }
+
+    return 0;
+}
