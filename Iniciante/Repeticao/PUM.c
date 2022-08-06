130acb3108ced978a578146a289e4f448d97b638
fredpaiva42 <fredericogdpaiva@gmail.com>
2022-08-06 14:45:22 -0300

    06/08/2022

---
+++ Iniciante/Repeticao/PUM.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int qtdLinhas;
+
+    scanf("%d", &qtdLinhas);
+
+    for (int j = 1; j <= (qtdLinhas * 4); j += 4){
+        printf("%d %d %d PUM\n", j, j+1, j+2);
+    }
+
+    return 0;
+}
